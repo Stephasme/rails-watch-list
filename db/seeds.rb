861f0182390c4c1951866adabd8e46bb7e1cabf3
stephane lorio <lorio.stephane@gmail.com>
2022-08-18 14:44:07 +0200

    finished seeds and faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+ puts "Getting rid of the movies"
+  @movie = Movie.destroy_all
+
+ puts "generating movies..."
+10.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Quote.yoda,
+    poster_url: Faker::Internet.url,
+    rating: (0..5).to_a.sample
+  )
+  puts "#{movie.title} was created" if movie.save!
+
+end
